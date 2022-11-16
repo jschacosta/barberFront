9e166967a1804d3798654b86929649cbcc7ea11f
Johann Schwarze <johannschwarze@Air-de-Johann.fritz.box>
2022-11-16 20:12:03 +0100

    api change

--- src/lib/index.js
+++ src/lib/index.js
@@ -5,10 +5,9 @@ const apis = new Map()
     // apis.set('prod', 'https://api.unabase.net')
     // apis.set('test', 'https://test-v4.herokuapp.com')
 apis.set('local', 'http://localhost:3000')
-apis.set('dev', 'https://zmsegiguxp.us-east-1.awsapprunner.com')
-apis.set('development', 'https://zmsegiguxp.us-east-1.awsapprunner.com')
-apis.set('test', 'https://apitv4.unabase.com')
-apis.set('production', 'https://apiv4.unabase.com')
+apis.set('dev', 'https://barberapi-production.up.railway.app')
+apis.set('development', 'https://barberapi-production.up.railway.app')
+apis.set('production', 'https://barberapi-production.up.railway.app')
 let env = ''
 
 if (localStorage.getItem('apiUrl')) {
--- src/state/store.js
+++ src/state/store.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import api from '../lib/api';
 
 import modules from './modules'
 
